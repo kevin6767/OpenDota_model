f741537cd99c335090601fe8bb99ad1368b15466
Kevin Eslick <eslick.kevin@yahoo.com>
2020-02-24 21:18:25 -0600

    playing around with state

--- src/components/Herodropdown.js
+++ src/components/Herodropdown.js
@@ -3,17 +3,15 @@ import React, { useState, useEffect } from "react";
 import {getStatsById} from '../services/heroes.service.js'
 
 
-const Herodropdown = () => {
-    const heroId = useState(
-        'Select an ID'
-    )
-    useEffect(getStatsById());
+const Herodropdown = (heroId) => {
+    const {stats, setStats} = useState();
+    useEffect( () => { setStats( getStatsById(heroId) ) }, [heroId] );
     return (
-        <h1> {heroId}</h1>
+        <>
+            <h2> {heroId}</h2>
 
-
-
-
-        )
+            stats: {stats}
+        </>
+    )
 }
 export default Herodropdown
